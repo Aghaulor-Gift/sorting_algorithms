5a9fb44704d49a43be60311ad8df7928b230b38d
Gift <aghaulor.gift@gmail.com>
2024-02-20 18:56:05 -0800

    2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,6 +11,7 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min_idx;
+	int temp, swap;
 
 	if (array == NULL || size < 2)
 	return;
@@ -18,15 +19,19 @@ void selection_sort(int *array, size_t size)
 	for (i = 0; i < size - 1; i++)
 	{
 	min_idx = i;
+	swap = 0;
 	for (j = i + 1; j < size; j++)
 	{
 	if (array[j] < array[min_idx])
-	min_idx = j;
+	{
+		min_idx = j;
+		swap = 1;
+	}
 	}
 
-	if (min_idx != i)
+	if (swap != 0)
  	{
-	int temp = array[i];
+	temp = array[i];
 	array[i] = array[min_idx];
 	array[min_idx] = temp;
 	print_array(array, size);
